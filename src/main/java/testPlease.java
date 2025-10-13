488577caffc918d27bae640006510873ee37d62b
Luke Hirst <lsh38@uni.brighton.ac.uk>
2025-10-13 12:31:49 +0100

    Initial commit on the TestingForFunBranch. Let's mess around with this and see what I can do..

--- src/main/java/testPlease.java
+++ src/main/java/testPlease.java
@@ -1,3 +1,4 @@
 public class testPlease {
     // blah blah blah blah please work blah blah blah blah :)
+    // The capital of Switzerland is Bern, despite what people believe. It is not Geneva or Zurich, like most people think.
 }
